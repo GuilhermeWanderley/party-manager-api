2985de1c462ef742cdb569374d6a3a4bac12ba72
Guilherme Wanderley <guilhermewanderleydev@gmail.com>
2026-05-14 12:17:13 -0300

    feat(client): add unique constraints to email and phone

--- src/main/java/com/example/party_manager/entity/Client.java
+++ src/main/java/com/example/party_manager/entity/Client.java
@@ -18,18 +18,22 @@ public class Client {
 
     @NotBlank(message = "Name is mandatory")
     @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    @Column(nullable = false, length = 100)
     private String name;
 
     @Email(message = "Email should be valid")
     @NotBlank(message = "Email is mandatory")
+    @Column(unique = true, nullable = false)
     private String email;
 
     @NotBlank(message = "Phone number is mandatory")
     @Pattern(regexp = "\\d{11}", message = "Phone number must be 11 digits")
+    @Column(unique = true, nullable = false, length = 11)
     private String phoneNumber;
 
     @Enumerated(EnumType.STRING)
     @NotNull(message = "User role is mandatory")
+    @Column(nullable = false)
     private UserRole userRole;
 
     public Client(String name, String email, String phoneNumber, UserRole userRole) {
